00743bda8dac76e63653f4009e5109b2298aae23
MattyK513 <136388707+MattyK513@users.noreply.github.com>
2023-10-05 16:36:21 -0500

    Completed HTML and JS

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
+const modeInput = document.getElementById('mode-input')
+const countInput = document.getElementById('count-input')
+
 document.getElementById('request-form')
     .addEventListener('submit', e => {
         e.preventDefault();
-        const color = document.getElementById('color-input').value.split("")
-        color.shift()
-        fetch(`https://www.thecolorapi.com/scheme?hex=${color.join("")}&`)
+        fetch(getUrlForFetch())
             .then(res => res.json())
             .then(data => {
-                const newColorArr = data.colors.map(color => color.hex.value)
-                console.log(newColorArr)
-                renderColorDisplay(newColorArr)
-            })
-    })
+                const newColorArr = data.colors.map(color => color.hex.value);
+                renderColorDisplay(newColorArr);
+            });
+    });
     
-    function renderColorDisplay(arr) {
-       const displayDiv = document.getElementById('color-display')
-       displayDiv.innerHTML = arr.map(color => `
-       <div class="color" style="background-color: ${color};"></div>
-       `).join("")
-    }
+modeInput.addEventListener("change", () => {
+    getCountOptions()
+})
+    
+function getUrlForFetch() {
+    const color = document.getElementById('color-input').value.split("");
+    color.shift();
+    return `https://www.thecolorapi.com/scheme?hex=${color.join("")}&mode=${modeInput.value}&count=${countInput.value}`;
+};
+    
+function renderColorDisplay(arr) {
+    const displayDiv = document.getElementById('color-display');
+    displayDiv.innerHTML = arr.map(color => `
+    <div class="color" style="background-color: ${color};"></div>
+    `).join("");
+};
+
+function getCountOptions() {
+     let countArr = [];
+    switch (modeInput.value) {
+        case ("monochrome"):
+        countArr = [10,9,8,7,6,5,4,3,2];
+        break;
+        case ("monochrome-dark"):
+        countArr = [7,6,5,4,3,2];
+        break;
+        case ("monochrome-light"):
+        countArr = [7,6,5,4,3,2];
+        break;
+        case ("analogic"):
+        countArr = [10,9,8,7,6,5,4,3,2];
+        break;
+        case ("complement"):
+        countArr = [2];
+        break;
+        case ("analogic-complement"):
+        countArr = [10,9,8,7,6,5,4,3,2];
+        break;
+        case ("triad"):
+        countArr = [3];
+        break;
+        case ("quad"):
+        countArr = [4];
+        break;
+    };
+    countInput.innerHTML = countArr.map(number => {
+        return number === 5 ? `<option selected>${number}</option>` : `<option>${number}</option>`
+    });
+};
